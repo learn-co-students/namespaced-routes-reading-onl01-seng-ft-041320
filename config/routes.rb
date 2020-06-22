e9bfd262cb089fde36bac4c55d3b1815b4356b9c
Tanattha <tanattha.thuenim@gmail.com>
2020-06-22 10:41:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
-
+  
+  #show on localhost
   root 'posts#index'
+
+  #get '/admin/stats', to: 'stats#index'
+  #scope allows us to group a block of routes with one prefix
+  #scope '/admin', module: 'admin' do
+    #resources :stats, only: [:index]
+  #end
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 end
